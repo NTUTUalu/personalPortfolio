fb43848bb1094172a6195a4bac19f27082128d90
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-04-11 13:46:09 +0200

    NOW showing active page

--- src/components/routes/home/navigation.jsx
+++ src/components/routes/home/navigation.jsx
@@ -1,39 +1,42 @@
-import NavigationStyles from  "./navigation.module.css";
-import "../home/navigation.module.css";
+import NavigationStyles from "./navigation.module.css";
 import lightIcon from "../../../../public/icons8-sun.svg";
 import darkIcon from "../../../../public/icons8-code.svg";
 import openNav from "../../../../public/x-menu.png";
 import closeNav from "../../../../public/menu.png";
-import "./home.module.css";
-import { Outlet,  Link} from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import Footer from '../footer/footer';
 
 function Navigation() {
+  // Get the current location using useLocation hook from react-router-dom
+  const location = useLocation();
 
   return (
     <>
-    <nav className={NavigationStyles.Navigation}>
-      <Link to={'/'}><img src={darkIcon} style={{width:'40px', height:'40px'}} className={NavigationStyles.goingHome}/></Link>
-      <div className={NavigationStyles.navLinks}>
-        <ul>
-          <li>
-            <Link to={`about/1`} className={NavigationStyles.links}>About</Link>
-          </li>
-          <li><Link to={`projects/1`}className={NavigationStyles.links}>Projects</Link></li>
-          <li><Link to={`contact/1`} className={NavigationStyles.links}>Contact</Link></li>
-          <img  src={lightIcon} />
-        </ul>
-       
+      <nav className={NavigationStyles.Navigation}>
+        <Link to={'/'}><img src={darkIcon} style={{width:'40px', height:'40px'}} className={NavigationStyles.goingHome}/></Link>
+        <div className={NavigationStyles.navLinks}>
+          <ul>
+            <li>
+              <Link to={`about/1`} className={`${NavigationStyles.links} ${location.pathname === '/about/1' ? NavigationStyles.activeLink : ''}`}>About</Link>
+            </li>
+            <li>
+              <Link to={`projects/1`} className={`${NavigationStyles.links} ${location.pathname === '/projects/1' ? NavigationStyles.activeLink : ''}`}>Projects</Link>
+            </li>
+            <li>
+              <Link to={`contact/1`} className={`${NavigationStyles.links} ${location.pathname === '/contact/1' ? NavigationStyles.activeLink : ''}`}>Contact</Link>
+            </li>
+          </ul>
+          <img src={lightIcon} />
+        </div>
         <img className={NavigationStyles.openNav} src={openNav}/>
         <img className={NavigationStyles.closeNav} src={closeNav}/>
+      </nav>
+      
+      <div id="detail">
+        <Outlet />
       </div>
-    </nav>
-    
-    <div id="detail">
-      <Outlet />
-    </div>
-    <Footer/>
-  </>
+      <Footer/>
+    </>
   );
 }
 
